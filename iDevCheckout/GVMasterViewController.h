c9261b17ef7c1564b658653fc95f4ea5ee2a20ca
Jonathan Engelsma <jengelsma@gmail.com>
2014-09-03 12:49:16 -0400

    Initial Commit

---
+++ iDevCheckout/GVMasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  GVMasterViewController.h
+//  iDevCheckout
+//
+//  Created by Jonathan Engelsma on 9/3/14.
+//  Copyright (c) 2014 Jonathan Engelsma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GVDetailViewController;
+
+@interface GVMasterViewController : UITableViewController
+
+@property (strong, nonatomic) GVDetailViewController *detailViewController;
+
+@end
